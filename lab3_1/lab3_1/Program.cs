82c30c4e014230c9cee36f79143e48fe219f91fd
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2023-11-12 21:06:20 +0300

    Add files via upload

---
+++ lab3_1/lab3_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace lab3_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new my_stack<int>();
+            void clear()
+            {
+                int stack_size = stack.Count;
+                for (int i = 0; i < stack_size; i++)
+                {
+                    stack.Pop();
+                }
+                Console.WriteLine("ok");
+            }
+
+            string answer = "";
+            while (answer != "exit")
+            {
+                answer = Console.ReadLine();
+                if (answer.Split().First() == "push")
+                {
+                    stack.Push(int.Parse(answer.Split().Last()));
+                }
+                if (answer == "size")
+                {
+                    Console.WriteLine($"{stack.Count}");
+                }
+                if (answer == "back")
+                {
+                    var item_back = stack.Back();
+                    Console.WriteLine($"{item_back}");
+                }
+                if (answer == "pop")
+                {
+                    var item_pop = stack.Pop();
+                    Console.WriteLine($"{item_pop}");
+                }
+                if (answer == "clear")
+                {
+                    clear();
+                }
+            }
+            Console.WriteLine("bye");
+        }
+    }
+}
---
+++ lab3_1/lab3_1/my_stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab3_1
+{
+    public class my_stack<T>
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count;    
+        public void Push(T item)
+        {
+            items.Add(item);
+            Console.WriteLine("ok");
+        }
+
+        public T Pop()
+        {
+            var item = top_item();
+            items.Remove(item);
+            return item;
+        }
+        public T Back()
+        {
+            var item = top_item();
+            return item;
+        }
+
+        private T top_item()
+        {
+            var item = items.LastOrDefault();
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пуст.");
+            }
+            return item;
+        }
+    }
+}
