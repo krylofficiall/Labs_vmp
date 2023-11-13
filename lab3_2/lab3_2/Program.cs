6cdb61f12f8a3fd417435920f11dc75124ad41d4
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2023-11-13 07:35:30 +0300

    Add files via upload

---
+++ lab3_2/lab3_2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace lab3_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            my_queue queue = new my_queue();
+            string answer = "";
+
+            while (answer != "exit")
+            {
+                answer = Console.ReadLine();
+                if (answer.Split().First() == "push")
+                {
+                    queue.Push(int.Parse(answer.Split().Last()));
+                    Console.WriteLine("ok");
+                }
+                if (answer == "pop")
+                {
+                    var item_pop = queue.Pop();
+                    Console.WriteLine(item_pop);
+                }
+                if (answer == "front")
+                {
+                    Console.WriteLine(queue.Front());
+                }
+                if (answer == "size")
+                {
+                    Console.WriteLine(queue.Size());
+                }
+                if (answer == "clear")
+                {
+                    queue.Clear();
+                    Console.WriteLine("ok");
+                }
+            }
+            Console.WriteLine("bye");
+        }
+
+    }
+
+}
---
+++ lab3_2/lab3_2/my_queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_2
+{
+    public class item
+    {
+        public int Value { get; }
+        public item Next { get; set; }
+        public item(int value)
+        {
+            Value = value;
+        }
+    }
+    public class my_queue
+    {
+        private item last;
+        private int size = 0;
+
+        public void Push(int value)
+        {
+            item new_item = new item(value);
+            item last_item = last;
+            if (size > 0)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (last_item.Next != null)
+                    {
+                        last_item = last_item.Next;
+                    }
+                    else
+                    {
+                        last_item.Next = new_item;
+                    }
+                }
+
+            }
+            else
+            {
+                last = new_item;
+            }
+            size++;
+        }
+        public int Pop()
+        {
+            int value = last.Value;
+            last = last.Next;
+            size--;
+            return value;
+        }
+        public int Front()
+        {
+            return last.Value;
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public void Clear()
+        {
+            last = null;
+            size = 0;
+        }
+    }
+}
