db81c9da65dae2b78355c0b16411dd8ee9147382
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2023-11-13 08:37:50 +0300

    Add files via upload

---
+++ lab3_3/lab3_3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace lab3_2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string answer = "))(()(";
+            int brackets = 0;
+            int position = -1;
+            bool flag = false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (brackets >= 0 && answer[i] == '(')
+                {
+                    brackets++;
+                }
+                if (brackets == -1)
+                {
+                    if (position == -1)
+                    {
+                        position = i;
+                    }
+                    brackets++;
+                    if (answer[i] == '(')
+                    {
+                        brackets++;
+                    }
+                }
+                if (answer[i] == ')')
+                {
+                    brackets--;
+                }
+                
+            }
+            if (brackets == 0)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+                if (position != -1)
+                {
+                    Console.WriteLine($"First excess close brackets in position: {position}");
+                }
+                if (brackets > 0)
+                {
+                    Console.WriteLine($"Count of excess open brackets: {brackets}");
+                }
+            }
+        }
+    }
+}
